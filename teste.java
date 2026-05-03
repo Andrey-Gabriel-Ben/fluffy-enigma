4783f58ec6ec07a35ffd574f304a73a5a6c56707
Andrey Gabriel Ben <andreygabrielben@gmail.com>
2026-05-03 17:54:44 -0300

    comit teste 2

--- teste.java
+++ teste.java
@@ -6,6 +6,6 @@ public class teste {
     public static void main(String[] args) {
         System.out.println("hello word");
         System.out.println("tudo bem");
-        System.out.println("teste");
+        System.out.println("teste 2 teste");
     }
 }   
